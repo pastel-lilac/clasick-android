36b1ba4ed9e84ca1b3e7137d504f88b4380caeef
batch <baaaakkrad@gmail.com>
2019-11-06 08:38:16 +0900

    [add]ClasickRepository and ClasickApi

--- app/src/main/java/com/pastel/lilac/clasick_android/ClasickApi.kt
+++ app/src/main/java/com/pastel/lilac/clasick_android/ClasickApi.kt
@@ -1,4 +1,13 @@
 package com.pastel.lilac.clasick_android
 
+import com.pastel.lilac.clasick_android.model.Music
+import com.pastel.lilac.clasick_android.model.Playlist
+import retrofit2.http.GET
+
 interface ClasickApi {
+    @GET("playlists")
+    suspend fun getPlayList(): List<Playlist>
+
+    @GET("musics")
+    suspend fun getMusic(): Music
 }
---
+++ app/src/main/java/com/pastel/lilac/clasick_android/repository/ClasickRepository.kt
@@ -0,0 +1,25 @@
+package com.pastel.lilac.clasick_android.repository
+
+import com.pastel.lilac.clasick_android.ClasickApi
+import com.pastel.lilac.clasick_android.model.Music
+import com.pastel.lilac.clasick_android.model.Playlist
+import com.squareup.moshi.Moshi
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+
+class ClasickRepository(baseURL: String) : ClasickApi {
+    private val apiClient = Retrofit.Builder()
+        .baseUrl(baseURL)
+        .addConverterFactory(MoshiConverterFactory.create(Moshi.Builder().build()))
+        .build()
+        .create(ClasickApi::class.java)
+
+    override suspend fun getPlayList(): List<Playlist> {
+        return apiClient.getPlayList()
+    }
+
+    override suspend fun getMusic(): Music {
+        return apiClient.getMusic()
+    }
+
+}
