3b8e875bac3cab99c035d6591dd8f3a74a179e04
batch <baaaakkrad@gmail.com>
2019-11-06 15:53:56 +0900

    [update]playlist vertical line and set cover image

--- app/src/main/java/com/pastel/lilac/clasick_android/playlist/PlaylistFragment.kt
+++ app/src/main/java/com/pastel/lilac/clasick_android/playlist/PlaylistFragment.kt
@@ -7,15 +7,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.pastel.lilac.clasick_android.R
+import com.pastel.lilac.clasick_android.model.Playlist
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import kotlinx.android.synthetic.main.fragment_playlist.*
+import java.sql.Driver
 
 class PlaylistFragment : Fragment() {
-
+    private lateinit var viewModel: PlaylistViewModel
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_playlist, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(PlaylistViewModel::class.java)
+        viewModel.fetchRemote()
+        observeViewModel()
+        val itemDecoration = DividerItemDecoration(context, DividerItemDecoration.VERTICAL)
+        playlistRecyclerView.addItemDecoration(itemDecoration)
+    }
+
+    private fun observeViewModel() {
+        viewModel.playlists.observe(this, Observer {
+            initRecyclerView(it.toPlaylistItem())
+        })
+    }
+
+    private fun initRecyclerView(playlistItem: List<PlaylistItem>) {
+        val pAdapter = GroupAdapter<GroupieViewHolder>().apply {
+            addAll(playlistItem)
+        }
+        playlistRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            setHasFixedSize(true)
+            adapter = pAdapter
+        }
+    }
+
+    fun List<Playlist>.toPlaylistItem() : List<PlaylistItem> {
+        return this.map {
+            PlaylistItem(it)
+        }
     }
 }
---
+++ app/src/main/java/com/pastel/lilac/clasick_android/playlist/PlaylistItem.kt
@@ -0,0 +1,15 @@
+package com.pastel.lilac.clasick_android.playlist
+
+import com.pastel.lilac.clasick_android.R
+import com.pastel.lilac.clasick_android.databinding.ItemPlaylistBinding
+import com.pastel.lilac.clasick_android.model.Playlist
+import com.xwray.groupie.databinding.BindableItem
+
+class PlaylistItem(private val playlist: Playlist) : BindableItem<ItemPlaylistBinding>() {
+    override fun getLayout() = R.layout.item_playlist
+
+    override fun bind(viewBinding: ItemPlaylistBinding, position: Int) {
+        viewBinding.playlist = playlist
+    }
+
+}
