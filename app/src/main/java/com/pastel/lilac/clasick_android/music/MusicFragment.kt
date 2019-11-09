cff6a4a60184d814043c6e76d1f1f286b1d77515
batch <baaaakkrad@gmail.com>
2019-11-09 17:41:14 +0900

    [update]Can retrieve Glide's cache

--- app/src/main/java/com/pastel/lilac/clasick_android/music/MusicFragment.kt
+++ app/src/main/java/com/pastel/lilac/clasick_android/music/MusicFragment.kt
@@ -1,63 +1,50 @@
 package com.pastel.lilac.clasick_android.music
 
-import android.media.Image
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.navArgs
 import com.bumptech.glide.Glide
-import com.pastel.lilac.clasick_android.BR
 
 import com.pastel.lilac.clasick_android.R
 import com.pastel.lilac.clasick_android.databinding.FragmentMusicBinding
-import kotlinx.android.synthetic.main.fragment_music.*
 import timber.log.Timber
 
 class MusicFragment : Fragment() {
     private lateinit var viewModel: MusicViewModel
     private lateinit var binding: FragmentMusicBinding
 
-    // リスト表示
-    // Glideキャッシュ確認
-    // navigationで値渡し確認(coverPath Url)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-//        this.binding = DataBindingUtil.inflate(inflater, R.layout.fragment_music, container, false)
         val view = inflater.inflate(R.layout.fragment_music, container, false)
         this.binding = FragmentMusicBinding.bind(view)
         viewModel = ViewModelProviders.of(this).get(MusicViewModel::class.java)
         this.observeViewModel()
-        var coverPath: String? = null
-        val coverImageView = requireActivity().findViewById<ImageView>(R.id.coverImageView)
-        arguments?.let {
-            coverPath = MusicFragmentArgs.fromBundle(it).coverPath
-            coverImageView?.let {
-                Glide.with(this)
-                    .load(coverPath)
-                    .onlyRetrieveFromCache(false)
-                    .into(it)
-            }
-        }
-//        return inflater.inflate(R.layout.fragment_music, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel.fetchRemote()
+        val coverImageView = view.findViewById<ImageView>(R.id.coverImageView)
+        val args: MusicFragmentArgs by navArgs()
+        val coverPath = args.coverPath
+        Glide.with(this)
+            .load(coverPath)
+            .onlyRetrieveFromCache(true)
+            .into(coverImageView)
+
     }
 
     private fun observeViewModel() {
         viewModel.musics.observe(this, Observer {
-            Timber.d(it[0].toString()) // itに値来てる
-//            this.binding.setVariable(BR.music, it)
             binding.music = it[0]
         })
     }
